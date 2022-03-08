54f46ff393b0b8d1a7ab25a82ac6ed020ad34ee8
azurepocmain <91505344+azurepocmain@users.noreply.github.com>
2022-03-08 08:40:58 -0700

    Update AzureFunctionSynapse_TempDB.ps1

--- AzureFunctionSynapse_TempDB.ps1
+++ AzureFunctionSynapse_TempDB.ps1
@@ -79,8 +79,7 @@ $SqlCmd.CommandText = " SELECT `
 FROM sys.dm_pdw_nodes_db_session_space_usage AS ssu `
     INNER JOIN sys.dm_pdw_nodes_exec_sessions AS es ON ssu.session_id = es.session_id AND ssu.pdw_node_id = es.pdw_node_id `
     INNER JOIN sys.dm_pdw_nodes_exec_connections AS er ON ssu.session_id = er.session_id AND ssu.pdw_node_id = er.pdw_node_id `
-    --INNER JOIN microsoft.vw_sql_requests AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id `
-	INNER JOIN sys.dm_pdw_exec_sessions exs on er.most_recent_session_id = exs.sql_spid `
+    INNER JOIN sys.dm_pdw_exec_sessions exs on er.most_recent_session_id = exs.sql_spid `
     INNER JOIN sys.dm_pdw_exec_requests exr on exr.request_id = exs.request_id AND exr.session_id=exs.session_id `
 WHERE DB_NAME(ssu.database_id) = 'tempdb' `
 AND exr.end_time IS  NULL `
@@ -174,8 +173,7 @@ $SqlCmd.CommandText = "SELECT `
 FROM sys.dm_pdw_nodes_db_session_space_usage AS ssu `
     INNER JOIN sys.dm_pdw_nodes_exec_sessions AS es ON ssu.session_id = es.session_id AND ssu.pdw_node_id = es.pdw_node_id `
     INNER JOIN sys.dm_pdw_nodes_exec_connections AS er ON ssu.session_id = er.session_id AND ssu.pdw_node_id = er.pdw_node_id `
-    --INNER JOIN microsoft.vw_sql_requests AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id `
-	INNER JOIN sys.dm_pdw_exec_sessions exs on er.most_recent_session_id = exs.sql_spid `
+    INNER JOIN sys.dm_pdw_exec_sessions exs on er.most_recent_session_id = exs.sql_spid `
     INNER JOIN sys.dm_pdw_exec_requests exr on exr.request_id = exs.request_id AND exr.session_id=exs.session_id `
 WHERE DB_NAME(ssu.database_id) = 'tempdb' `
 AND exr.end_time IS  NULL `
