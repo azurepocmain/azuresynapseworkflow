53a74ea43689f83ab05c69b17eaa9eec4fc95f5d
azurepocmain <91505344+azurepocmain@users.noreply.github.com>
2022-03-07 18:47:37 -0700

    Update AzureAutomationSynapse_Tempdb.ps1

--- AzureAutomationSynapse_Tempdb.ps1
+++ AzureAutomationSynapse_Tempdb.ps1
@@ -12,12 +12,8 @@ try {
 
 
 
-
 ###Context no longer needed as we will get the Synapse SQL Pool instance name from the config parameter.###
 
-### Set-AzContext -SubscriptionId $env:azpocsub
-
-#$SQLDW=@($env:AzureSynapse1);
 
 
 ##You can remove the below in Prod if you like after testing#####
@@ -60,18 +56,19 @@ $SqlConnection.AccessToken = $AccessToken
 
 $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
 
-$SqlCmd.CommandText = " SELECT `
-Count(1) AS TOTAL  `
+$SqlCmd.CommandText = "SELECT `
+ Count(1) AS TOTAL  `
 FROM sys.dm_pdw_nodes_db_session_space_usage AS ssu `
     INNER JOIN sys.dm_pdw_nodes_exec_sessions AS es ON ssu.session_id = es.session_id AND ssu.pdw_node_id = es.pdw_node_id `
     INNER JOIN sys.dm_pdw_nodes_exec_connections AS er ON ssu.session_id = er.session_id AND ssu.pdw_node_id = er.pdw_node_id `
-    INNER JOIN microsoft.vw_sql_requests AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id `
-    INNER JOIN sys.dm_pdw_exec_requests exr on exr.request_id = sr.request_id `
-    LEFT JOIN sys.dm_pdw_exec_sessions exs on exr.session_id = exs.session_id `
+    --INNER JOIN microsoft.vw_sql_requests AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id `
+	INNER JOIN sys.dm_pdw_exec_sessions exs on er.most_recent_session_id = exs.sql_spid `
+    INNER JOIN sys.dm_pdw_exec_requests exr on exr.request_id = exs.request_id AND exr.session_id=exs.session_id `
 WHERE DB_NAME(ssu.database_id) = 'tempdb' `
 AND exr.end_time IS  NULL `
     AND es.session_id <> @@SPID `
-    AND es.login_name <> 'sa' ; "
+    AND es.login_name <> 'sa'`
+	AND  (ssu.user_objects_alloc_page_count * 8)  IS NOT NULL; "
 
 $SqlCmd.Connection = $SqlConnection
 
@@ -138,12 +135,13 @@ $SqlConnection.AccessToken = $AccessToken
 $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
 
 $SqlCmd.CommandText = "SELECT `
-    sr.request_id, `
+    exr.request_id, `
 	exr.submit_time, `
     ssu.session_id, `
     ssu.pdw_node_id, `
-    sr.command, `
-    sr.total_elapsed_time, `
+    exr.command, `
+    exr.start_time, `
+	exr.end_time, `
     exs.login_name AS 'LoginName', `
     DB_NAME(ssu.database_id) AS 'DatabaseName', `
     (es.memory_usage * 8) AS 'MemoryUsage_in_KB', `
@@ -159,13 +157,14 @@ $SqlCmd.CommandText = "SELECT `
 FROM sys.dm_pdw_nodes_db_session_space_usage AS ssu `
     INNER JOIN sys.dm_pdw_nodes_exec_sessions AS es ON ssu.session_id = es.session_id AND ssu.pdw_node_id = es.pdw_node_id `
     INNER JOIN sys.dm_pdw_nodes_exec_connections AS er ON ssu.session_id = er.session_id AND ssu.pdw_node_id = er.pdw_node_id `
-    INNER JOIN microsoft.vw_sql_requests AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id `
-    INNER JOIN sys.dm_pdw_exec_requests exr on exr.request_id = sr.request_id `
-    LEFT JOIN sys.dm_pdw_exec_sessions exs on exr.session_id = exs.session_id `
+    --INNER JOIN microsoft.vw_sql_requests AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id `
+	INNER JOIN sys.dm_pdw_exec_sessions exs on er.most_recent_session_id = exs.sql_spid `
+    INNER JOIN sys.dm_pdw_exec_requests exr on exr.request_id = exs.request_id AND exr.session_id=exs.session_id `
 WHERE DB_NAME(ssu.database_id) = 'tempdb' `
 AND exr.end_time IS  NULL `
     AND es.session_id <> @@SPID `
-    AND es.login_name <> 'sa';"
+    AND es.login_name <> 'sa'`
+	AND  (ssu.user_objects_alloc_page_count * 8)  IS NOT NULL; "
 
 $SqlCmd.Connection = $SqlConnection
 
