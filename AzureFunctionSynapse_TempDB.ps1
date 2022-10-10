4e6e7b52f4dae709fd3a8281e1407669a6e20c2c
azurepocmain <91505344+azurepocmain@users.noreply.github.com>
2022-10-09 17:07:46 -0700

    Update AzureFunctionSynapse_TempDB.ps1
    Altered the statement to get only active TempDB usage.

--- AzureFunctionSynapse_TempDB.ps1
+++ AzureFunctionSynapse_TempDB.ps1
@@ -74,20 +74,38 @@ $SqlConnection.AccessToken = $AccessToken
 
 $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
 
-$SqlCmd.CommandText = "SELECT  Count(1) AS TOTAL `
-FROM sys.dm_pdw_nodes_db_session_space_usage AS ssu  `
-INNER JOIN sys.dm_pdw_nodes_exec_sessions AS es ON ssu.session_id = es.session_id AND ssu.pdw_node_id = es.pdw_node_id  `
-INNER JOIN sys.dm_pdw_nodes_exec_connections AS er ON ssu.session_id = er.session_id AND ssu.pdw_node_id = er.pdw_node_id  `
-INNER JOIN microsoft.vw_sql_requests AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id  `
-LEFT JOIN sys.dm_pdw_exec_requests exr on exr.request_id = sr.request_id  `
-LEFT JOIN sys.dm_pdw_exec_sessions exs on exr.session_id = exs.session_id  `
-WHERE   `
-exr.end_time IS  NULL  `
-AND  `
-DB_NAME(ssu.database_id) = 'tempdb'  `
-AND exs.session_id <> session_id()  `
-AND es.login_name <> 'sa'  `
-AND  (ssu.user_objects_alloc_page_count * 8) <> 0 or (ssu.internal_objects_alloc_page_count * 8) <> 0;"
+$SqlCmd.CommandText = "SELECT Count(1) AS TOTAL `
+FROM sys.dm_pdw_nodes_db_session_space_usage AS ssu `
+INNER JOIN sys.dm_pdw_nodes_exec_sessions AS es ON ssu.session_id = es.session_id AND ssu.pdw_node_id = es.pdw_node_id `
+INNER JOIN sys.dm_pdw_nodes_exec_connections AS er ON ssu.session_id = er.session_id AND ssu.pdw_node_id = er.pdw_node_id `
+INNER JOIN ( `
+SELECT `
+sr.request_id, `
+sr.step_index, `
+(CASE WHEN (sr.distribution_id = -1 ) THEN (SELECT pdw_node_id FROM sys.dm_pdw_nodes WHERE type = 'CONTROL') ELSE d.pdw_node_id END) AS pdw_node_id, `
+sr.distribution_id, `
+sr.status, `
+sr.error_id, `
+sr.start_time, `
+sr.end_time, `
+sr.total_elapsed_time, `
+sr.row_count, `
+sr.spid, `
+sr.command `
+FROM `
+sys.pdw_distributions AS d `
+RIGHT JOIN sys.dm_pdw_sql_requests AS sr ON d.distribution_id = sr.distribution_id `
+) AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id `
+LEFT JOIN sys.dm_pdw_exec_requests exr on exr.request_id = sr.request_id `
+LEFT JOIN sys.dm_pdw_exec_sessions exs on exr.session_id = exs.session_id `
+WHERE `
+DB_NAME(ssu.database_id) = 'tempdb' `
+AND es.session_id <> @@SPID `
+AND exs.session_id <> session_id() `
+AND es.login_name <> 'sa' `
+AND exs.login_name <> 'System' `
+AND es.is_user_process = 1 `
+AND exr.[end_time] IS NULL;"
 
 $SqlCmd.Connection = $SqlConnection
 
@@ -152,39 +170,71 @@ $SqlConnection.AccessToken = $AccessToken
 
 $SqlCmd = New-Object System.Data.SqlClient.SqlCommand
 
-$SqlCmd.CommandText = "SELECT  `
-sr.request_id,  `
-ssu.session_id,  `
-ssu.pdw_node_id,  `
-        exr.submit_time,  `
-        exr.start_time,  `
-        exr.end_time,  `
-exr.command,  `
-sr.total_elapsed_time,  `
-exs.login_name AS 'loginName', `
-DB_NAME(ssu.database_id) AS 'DatabaseName', `
-   (ssu.user_objects_alloc_page_count * 8) AS 'Space_Allocated_For_User_Objects_KB',  `
-(ssu.user_objects_dealloc_page_count * 8) AS 'Space_Deallocated_For_User_Objects_KB',  `
-(ssu.internal_objects_alloc_page_count * 8) AS 'Space_Allocated_For_Internal_Objects_KB',  `
-(ssu.internal_objects_dealloc_page_count * 8) AS 'Space_Deallocated_For_Internal_Objects_KB',  `
-CASE es.is_user_process  `
-WHEN 1 THEN 'User Session'  `
-WHEN 0 THEN 'System Session'  `
-END AS 'SessionType',  `
-es.row_count AS 'RowCount'  `
-FROM sys.dm_pdw_nodes_db_session_space_usage AS ssu  `
-INNER JOIN sys.dm_pdw_nodes_exec_sessions AS es ON ssu.session_id = es.session_id AND ssu.pdw_node_id = es.pdw_node_id  `
-INNER JOIN sys.dm_pdw_nodes_exec_connections AS er ON ssu.session_id = er.session_id AND ssu.pdw_node_id = er.pdw_node_id  `
-INNER JOIN microsoft.vw_sql_requests AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id  `
-LEFT JOIN sys.dm_pdw_exec_requests exr on exr.request_id = sr.request_id  `
-LEFT JOIN sys.dm_pdw_exec_sessions exs on exr.session_id = exs.session_id  `
-WHERE   `
-exr.end_time IS  NULL  `
-AND  `
-DB_NAME(ssu.database_id) = 'tempdb'  `
-AND exs.session_id <> session_id()   `
-AND es.login_name <> 'sa'  `
-AND  (ssu.user_objects_alloc_page_count * 8) <> 0 or (ssu.internal_objects_alloc_page_count * 8) <> 0;"
+$SqlCmd.CommandText = "SELECT `
+exr.request_id `
+,exr.session_id `
+,exr.command `
+,exr.[label] `
+,exr.[status] `
+,exr.[submit_time] `
+,exr.[start_time] `
+,exr.[end_time] `
+,CONVERT(numeric(25,3),DATEDIFF(ms,exr.[submit_time],exr.[start_time]) / 1000.0) [Request_queue_time_sec]`
+,CONVERT(numeric(25,3),DATEDIFF(ms,exr.[end_compile_time],exr.[start_time]) / 1000.0) AS [Request_compile_time_sec]`
+,CONVERT(numeric(25,3),DATEDIFF(ms,exr.[end_compile_time],exr.[end_time]) / 1000.0) AS [Request_execution_time_sec]`
+,CONVERT(numeric(25,2),exr.[total_Elapsed_time] / 1000.0) AS [Total_Elapsed_time_sec]`
+,CONVERT(numeric(25,2),exr.[total_Elapsed_time] / 1000.0 / 60 ) AS [Total_Elapsed_time_min]`
+,exs.login_name AS [loginName]`
+,[MemoryUsage (in KB)] = SUM((es.memory_usage * 8)) `
+,CONVERT(numeric(25,2),SUM((ssu.user_objects_alloc_page_count * 8)) / 1024.0) AS [Space_Allocated_For_User_Objects_MB]`
+,CONVERT(numeric(25,2),SUM((ssu.internal_objects_alloc_page_count * 8)) / 1024.0) AS [Space_Allocated_For_Internal_Objects_MB] `
+,[RowCount] = SUM(es.row_count) `
+FROM sys.dm_pdw_nodes_db_session_space_usage AS ssu `
+INNER JOIN sys.dm_pdw_nodes_exec_sessions AS es ON ssu.session_id = es.session_id AND ssu.pdw_node_id = es.pdw_node_id `
+INNER JOIN sys.dm_pdw_nodes_exec_connections AS er ON ssu.session_id = er.session_id AND ssu.pdw_node_id = er.pdw_node_id `
+INNER JOIN ( `
+SELECT `
+sr.request_id, `
+sr.step_index, `
+(CASE WHEN (sr.distribution_id = -1 ) THEN (SELECT pdw_node_id FROM sys.dm_pdw_nodes WHERE type = 'CONTROL') ELSE d.pdw_node_id END) AS pdw_node_id, `
+sr.distribution_id, `
+sr.status, `
+sr.error_id, `
+sr.start_time, `
+sr.end_time, `
+sr.total_elapsed_time, `
+sr.row_count, `
+sr.spid, `
+sr.command `
+FROM `
+sys.pdw_distributions AS d `
+RIGHT JOIN sys.dm_pdw_sql_requests AS sr ON d.distribution_id = sr.distribution_id `
+) AS sr ON ssu.session_id = sr.spid AND ssu.pdw_node_id = sr.pdw_node_id `
+LEFT JOIN sys.dm_pdw_exec_requests exr on exr.request_id = sr.request_id `
+LEFT JOIN sys.dm_pdw_exec_sessions exs on exr.session_id = exs.session_id `
+WHERE `
+DB_NAME(ssu.database_id) = 'tempdb' `
+AND es.session_id <> @@SPID `
+AND exs.session_id <> session_id() `
+AND es.login_name <> 'sa' `
+AND exs.login_name <> 'System' `
+AND es.is_user_process = 1 `
+AND exr.[end_time] IS NULL `
+GROUP BY `
+exr.request_id `
+,exr.session_id `
+,exr.command `
+,exr.[label] `
+,exr.[status] `
+,exr.[submit_time] `
+,exr.[start_time] `
+,exr.[end_time] `
+,CONVERT(numeric(25,3),DATEDIFF(ms,exr.[submit_time],exr.[start_time]) / 1000.0) `
+,CONVERT(numeric(25,3),DATEDIFF(ms,exr.[end_compile_time],exr.[start_time]) / 1000.0) `
+,CONVERT(numeric(25,3),DATEDIFF(ms,exr.[end_compile_time],exr.[end_time]) / 1000.0) `
+,CONVERT(numeric(25,2),exr.[total_Elapsed_time] / 1000.0) `
+,CONVERT(numeric(25,2),exr.[total_Elapsed_time] / 1000.0 / 60 ) `
+,exs.login_name;"
 
 $SqlCmd.Connection = $SqlConnection
 
@@ -201,7 +251,7 @@ $SqlConnection.Close()
 
 ###Convert the data to JSon directly and select the specific objects needed from the above query, all objects are selected in this case, but you can omit any if needed###
 
-$SynapsePOC=$dataset | Select-Object request_id, loginName, session_id, submit_time,   start_time, end_time,  command,  Space_Allocated_For_User_Objects_KB, Space_Deallocated_For_User_Objects_KB, Space_Allocated_For_Internal_Objects_KB, Space_Deallocated_For_Internal_Objects_KB,  SessionType, RowCount  |ConvertTo-Json
+$SynapsePOC=$dataset | Select-Object request_id, loginName, session_id, submit_time,   start_time, end_time,  command,  Space_Allocated_For_User_Objects_MB,  Space_Allocated_For_Internal_Objects_MB, MemoryUsage,  RowCount, Total_Elapsed_time_min  |ConvertTo-Json
 
 
 
