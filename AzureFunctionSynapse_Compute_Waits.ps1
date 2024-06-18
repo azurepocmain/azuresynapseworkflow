b2b4e4642ce0486876a79d94dfbc58026bdd9e1e
azurepocmain <91505344+azurepocmain@users.noreply.github.com>
2024-06-17 20:20:57 -0700

    Update AzureFunctionSynapse_Compute_Waits.ps1
    added and pwsess.session_id <> session_id() to remove azure function calls from results.

--- AzureFunctionSynapse_Compute_Waits.ps1
+++ AzureFunctionSynapse_Compute_Waits.ps1
@@ -90,6 +90,7 @@ JOIN sys.dm_pdw_nodes_exec_text_query_plan execplan on execplan.session_id=nodee
 JOIN  sys.dm_pdw_exec_requests execreq on execreq.request_id=sqlrequest.request_id `
 JOIN  sys.dm_pdw_exec_sessions pwsess ON  pwsess.session_id=execreq.session_id `
 where execreq.status NOT IN ('Cancelled', 'Completed', 'Failed' ) `
+and pwsess.session_id <> session_id() `
 group by nodeexreq.wait_type,  execreq.request_id, execreq.command, execplan.query_plan, pwsess.Login_Name, pwsess.Login_Time, pwsess.status, pwsess.query_count, execreq.session_id, pwsess.app_name, nodeexreq.wait_resource, nodeexreq.open_transaction_count, nodeexreq.blocking_session_id `
 ,nodeexreq.pdw_node_id,nodeexreq.session_id"
 
